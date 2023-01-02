75bd2019753c6e41715d6522eb7ff222d612397b
Akilah Powell <atpjun@gmail.com>
2023-01-02 07:56:54 -0400

    album

--- profile-feed/music-player.js
+++ profile-feed/music-player.js
@@ -34,3 +34,20 @@ export async function renderMenuBar() {
     return content;
 }
 
+export function getSongs() {
+    const songs = [profile - feed\music - player.js
+        assets\EL U╠üLTIMO TOUR DEL MUNDO\01 EL MUNDO ES MI╠üO.mp3
+        assets\EL U╠üLTIMO TOUR DEL MUNDO\02 TE MUDASTE.mp3
+        assets\EL U╠üLTIMO TOUR DEL MUNDO\04 MALDITA POBREZA.mp3
+        assets\EL U╠üLTIMO TOUR DEL MUNDO\07 YO VISTO ASI╠ü.mp3
+        assets\EL U╠üLTIMO TOUR DEL MUNDO\08 HACIENDO QUE ME AMAS.mp3
+        assets\EL U╠üLTIMO TOUR DEL MUNDO\09 BOOKER T.mp3
+        assets\EL U╠üLTIMO TOUR DEL MUNDO\10 LA DROGA.mp3
+        assets\EL U╠üLTIMO TOUR DEL MUNDO\11 DA╠üKITI(ft Jhay Cortez).mp3
+        assets\EL U╠üLTIMO TOUR DEL MUNDO\12 TRELLAS.mp3
+        assets\EL U╠üLTIMO TOUR DEL MUNDO\13 SORRY PAPI(ft ABRA).mp3
+        assets\EL U╠üLTIMO TOUR DEL MUNDO\14 120.mp3
+        assets\EL U╠üLTIMO TOUR DEL MUNDO\15 ANTES QUE SE ACABE.mp3
+        assets\EL U╠üLTIMO TOUR DEL MUNDO\16 CANTARES DE NAVIDAD.mp3]
+}
+
