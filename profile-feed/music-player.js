29f62eb4d32dde979fedf871c5d27b8355de3ddb
Akilah Powell <atpjun@gmail.com>
2023-01-02 08:58:07 -0400

    put the scrip tag in the index

--- profile-feed/music-player.js
+++ profile-feed/music-player.js
@@ -34,20 +34,34 @@ export async function renderMenuBar() {
     return content;
 }
 
-export function getSongs() {
-    const songs = [profile - feed\music - player.js
-        assets\EL U╠üLTIMO TOUR DEL MUNDO\01 EL MUNDO ES MI╠üO.mp3
-        assets\EL U╠üLTIMO TOUR DEL MUNDO\02 TE MUDASTE.mp3
-        assets\EL U╠üLTIMO TOUR DEL MUNDO\04 MALDITA POBREZA.mp3
-        assets\EL U╠üLTIMO TOUR DEL MUNDO\07 YO VISTO ASI╠ü.mp3
-        assets\EL U╠üLTIMO TOUR DEL MUNDO\08 HACIENDO QUE ME AMAS.mp3
-        assets\EL U╠üLTIMO TOUR DEL MUNDO\09 BOOKER T.mp3
-        assets\EL U╠üLTIMO TOUR DEL MUNDO\10 LA DROGA.mp3
-        assets\EL U╠üLTIMO TOUR DEL MUNDO\11 DA╠üKITI(ft Jhay Cortez).mp3
-        assets\EL U╠üLTIMO TOUR DEL MUNDO\12 TRELLAS.mp3
-        assets\EL U╠üLTIMO TOUR DEL MUNDO\13 SORRY PAPI(ft ABRA).mp3
-        assets\EL U╠üLTIMO TOUR DEL MUNDO\14 120.mp3
-        assets\EL U╠üLTIMO TOUR DEL MUNDO\15 ANTES QUE SE ACABE.mp3
-        assets\EL U╠üLTIMO TOUR DEL MUNDO\16 CANTARES DE NAVIDAD.mp3]
+// export function getSongs() {
+//     const songs = [
+//         ''assets\EUTDM\01 EL MUNDO ES MIO.mp3'
+//         'assets\EUTDM\02 TE MUDASTE.mp3
+//         'assets\EUTDM\03 HOY COBRE.mp3
+//         'assets\EUTDM\04 MALDITA POBREZA.mp3
+//         'assets\EUTDM\05 LA NOCHE DE ANOCHE(ft ROSALIA).mp3
+//         'assets\EUTDM\06 TE DESEO LO MEJOR.mp3
+//         'assets\EUTDM\07 YO VISTO ASI.mp3
+//         'assets\EUTDM\08 HACIENDO QUE ME AMAS.mp3
+//         'assets\EUTDM\09 BOOKER T.mp3
+//         'assets\EUTDM\10 LA DROGA.mp3
+//         'assets\EUTDM\11 DAKITI(ft Jhay Cortez).mp3
+//         'assets\EUTDM\12 TRELLAS.mp3
+//         'assets\EUTDM\13 SORRY PAPI(ft ABRA).mp3
+//         'assets\EUTDM\14 120.mp3
+//         'assets\EUTDM\15 ANTES QUE SE ACABE.mp3
+//         'assets\EUTDM\16 CANTARES DE NAVIDAD.mp3]
+// }
+
+export function renderMusicCover() {
+    const covers = [{}];
 }
 
+// display their magazines function for images and function for video
+// those functions will replace playlists and will add fourth
+
+// middle
+export function profileSongs() {
+    const mySwiper = new Swiper
+}
